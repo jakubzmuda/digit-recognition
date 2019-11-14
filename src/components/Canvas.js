e264444fd87fb30ba0e8e5365a27a3b557e9bd0a
Jakub Żmuda <goodguykuba@gmail.com>
2019-11-14 10:03:16 +0100

    canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import styles from './Canvas.module.css';
 
 export default class Canvas extends Component {
+
+  componentDidMount() {
+    this.draw()
+  }
+
   render() {
     return (
       <div className={styles.container}>
-
+        <canvas
+          ref={'canvas'}
+          width="400"
+          height="400"
+        />
       </div>
     );
+  }
+
+  draw() {
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.fillRect(0,0, 400, 400);
   }
 }
 
