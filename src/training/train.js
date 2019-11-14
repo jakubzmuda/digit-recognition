588f02ff6458bff591475d8b005cb502d6d84470
Jakub Żmuda <goodguykuba@gmail.com>
2019-11-14 16:17:36 +0100

    learning WIP

--- src/training/train.js
+++ src/training/train.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 
 console.log('training');
 
-const directoryPath = './src/data/training/0';
-fs.readdir(directoryPath, function (err, files) {
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-  files.forEach(function (file) {
-    console.log(file);
+for(let i=0;i<=9;i++) {
+  learnDigit(i);
+}
+
+function learnDigit(digit) {
+  const directoryPath = `./src/data/training/${digit}`;
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+      console.log(file);
+    });
   });
-});
+}
