2d8df5bd2da78e100bbf49765572c910bdfdcd4a
Jakub Żmuda <goodguykuba@gmail.com>
2019-11-14 16:15:03 +0100

    iterating through dir

--- src/training/train.js
+++ src/training/train.js
@@ -1,3 +1,14 @@
+const path = require('path');
 const fs = require('fs');
 
 console.log('training');
+
+const directoryPath = './src/data/training/0';
+fs.readdir(directoryPath, function (err, files) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  files.forEach(function (file) {
+    console.log(file);
+  });
+});
